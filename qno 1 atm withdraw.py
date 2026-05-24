2474c1712c3244f9efaae3cda52692fee5aa3777
sampanna170-a11y <sampanna170@gmail.com>
2026-05-24 22:35:25 +0545

    Update qno 1 atm withdraw.py

--- qno 1 atm withdraw.py
+++ qno 1 atm withdraw.py
@@ -2,3 +2,5 @@
 balance = float(input("Enter current account balance (NPR): "))
 daily_withdrawn = float(input("Enter total amount already withdrawn today (NPR): "))
 amount = float(input("Enter withdrawal amount (NPR): "))
+
+daily_limit = 50000
