42a3998f8df1e52a8475e86bec4051176d653f7b
sampanna170-a11y <sampanna170@gmail.com>
2026-05-24 23:30:01 +0545

    Update qno4 password strength checker.py

--- qno4 password strength checker.py
+++ qno4 password strength checker.py
@@ -1,1 +1,10 @@
 # Password Strength Checker
+passwords = ["hello", "Hello123", "H3ll0@World", "12345678", "MyP@ss!"]
+
+special_chars = "!@#$%^&*"
+
+for password in passwords:
+
+    print("\nChecking password:", password)
+
+    missing = []
