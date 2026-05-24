13407e2512b377c62c70066a36d5a207c62e625b
sampanna170-a11y <sampanna170@gmail.com>
2026-05-24 22:34:44 +0545

    float daily withdraw

--- qno 1 atm withdraw.py
+++ qno 1 atm withdraw.py
@@ -1,1 +1,4 @@
 # ATM Withdrawal Validator
+balance = float(input("Enter current account balance (NPR): "))
+daily_withdrawn = float(input("Enter total amount already withdrawn today (NPR): "))
+amount = float(input("Enter withdrawal amount (NPR): "))
