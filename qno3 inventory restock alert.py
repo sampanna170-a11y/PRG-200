40554cbb6832b127a23c425eb9d32040f8c72693
sampanna170-a11y <sampanna170@gmail.com>
2026-05-24 23:26:56 +0545

    Update qno3 inventory restock alert.py

--- qno3 inventory restock alert.py
+++ qno3 inventory restock alert.py
@@ -1,1 +1,9 @@
 # Inventory Restock Alert
+inventory = [
+    {"item": "Rice", "stock": 5, "threshold": 10},
+    {"item": "Eggs", "stock": 24, "threshold": 12},
+    {"item": "Milk", "stock": 3, "threshold": 6},
+    {"item": "Bread", "stock": 8, "threshold": 5},
+    {"item": "Chicken", "stock": 0, "threshold": 4},
+    {"item": "Cooking Oil", "stock": 2, "threshold": 3},
+]
